41c86aeddb3c4cbb486264abca01fc4d17e294f5
LiveIce <ptjekovski@gmail.com>
2026-02-12 19:59:15 +0100

    Update data.js

--- data.js
+++ data.js
@@ -641,6 +641,8 @@ const data = {
       { number: 00, title: "Koen Casteels", desc: "<b>Koen Casteels</b><br>25 juni 1992 i Bonheiden, Belgien", img: "https://raw.githubusercontent.com/LiveIce/collector-data/refs/heads/main/fotboll/topps/Kort0b.jpg", extra: "Målvakt - VfL Wolfsburg" },
       { number: 1, title: "Felix Uduokhai", desc: "<b>Felix Ohis Uduokhai</b><br>9 september 1997 i Annaberg-Buchholz, Tyskland", img: "https://raw.githubusercontent.com/LiveIce/collector-data/refs/heads/main/fotboll/topps/Kort1.jpg", extra: "Försvarare- FC Augsburg" },
       { number: 2, title: "Tobias Strobl", desc: "<b>Tobias Strobl</b><br>12 maj 1990 i München, Tyskland", img: "https://raw.githubusercontent.com/LiveIce/collector-data/refs/heads/main/fotboll/topps/Kort2.jpg", extra: "Mittfältare - FC Augsburg" },
+      { number: 13, title: "Jurgen Ekkelenkamp", desc: "<b>Jurgen Ekkelenkamp</b><br>5 april 2000 i Zeist, Nederländerna", img: "https://raw.githubusercontent.com/LiveIce/collector-data/refs/heads/main/fotboll/topps/Kort13.jpg", extra: "Mittfältare - Hertha Berlin" },
+      { number: 14, title: "Niklas Stark", desc: "<b>Niklas Stark</b><br>14 april 1995 i Neustadt an der Aisch, Tyskland", img: "https://raw.githubusercontent.com/LiveIce/collector-data/refs/heads/main/fotboll/topps/Kort14.jpg", extra: "Försvarare - Hertha Berlin" },
       { number: 21, title: "Max Kruse", desc: "<b>Max Bennet Kruse</b><br>19 mars 1988 i Reinbek, Tyskland", img: "https://raw.githubusercontent.com/LiveIce/collector-data/refs/heads/main/fotboll/topps/Kort21.jpg", extra: "Anfallare - 1. FC Union Berlin" },
       { number: 22, title: "Marvin Friedrich", desc: "<b>Marvin Friedrich</b><br>13 december 1995 i Kassel, Tyskland", img: "https://raw.githubusercontent.com/LiveIce/collector-data/refs/heads/main/fotboll/topps/Kort22.jpg", extra: "Försvarare - Borussia Mönchengladbach" },
       { number: 23, title: "Sheraldo Becker", desc: "<b>Sheraldo Rudi Salomo Willem Becker</b><br>9 februari 1995 i Amsterdam, Nederländerna", img: "https://raw.githubusercontent.com/LiveIce/collector-data/refs/heads/main/fotboll/topps/Kort23.jpg", extra: "Anfallare - 1. FC Union Berlin" },
@@ -834,6 +836,7 @@ const data = {
   }
 
 };
+
 
 
 
