805489074ec6c2b6d099efbbf1d6804120c04133
LiveIce <ptjekovski@gmail.com>
2026-02-22 20:46:50 +0100

    Update data.js

--- data.js
+++ data.js
@@ -644,6 +644,8 @@ const data = {
       { number: 00, title: "Koen Casteels", desc: "<b>Koen Casteels</b><br>25 juni 1992 i Bonheiden, Belgien", img: "https://raw.githubusercontent.com/LiveIce/collector-data/refs/heads/main/fotboll/topps/Kort0b.jpg", extra: "Målvakt - VfL Wolfsburg" },
       { number: 00, title: "Branimir Hrgota", desc: "<b>Branimir Hrgota</b><br>12 januari 1993 i Zenica, Bosnien och Hercegovina", img: "https://raw.githubusercontent.com/LiveIce/collector-data/refs/heads/main/fotboll/topps/Kort0c.jpg", extra: "Anfallare - SpVgg Greuther Fürth" },
       { number: 00, title: "Dedryck Boyata", desc: "<b>Anga Dedryck Boyata</b><br>28 november 1990 i Uccle, Belgien", img: "https://raw.githubusercontent.com/LiveIce/collector-data/refs/heads/main/fotboll/topps/Kort0d.jpg", extra: "Mittfältare - Hertha Berlin" },
+      { number: 00, title: "Péter Gulácsi", desc: "<b>Péter Gulácsi</b><br>6 maj 1990 i Budapest, Ungern", img: "https://raw.githubusercontent.com/LiveIce/collector-data/refs/heads/main/fotboll/topps/Kort0e.jpg", extra: "Målvakt - RB Leipzig" },
+      { number: 00, title: "Wataru Endo", desc: "<b>Wataru Endo</b><br>9 februari 1993 i Totsuka Ward, Yokohama, Kanagawa prefektur, Japan", img: "https://raw.githubusercontent.com/LiveIce/collector-data/refs/heads/main/fotboll/topps/Kort0f.jpg", extra: "Mittfältare - VfB Stuttgart" },
       { number: 1, title: "Felix Uduokhai", desc: "<b>Felix Ohis Uduokhai</b><br>9 september 1997 i Annaberg-Buchholz, Tyskland", img: "https://raw.githubusercontent.com/LiveIce/collector-data/refs/heads/main/fotboll/topps/Kort1.jpg", extra: "Försvarare- FC Augsburg" },
       { number: 2, title: "Tobias Strobl", desc: "<b>Tobias Strobl</b><br>12 maj 1990 i München, Tyskland", img: "https://raw.githubusercontent.com/LiveIce/collector-data/refs/heads/main/fotboll/topps/Kort2.jpg", extra: "Mittfältare - FC Augsburg" },
       { number: 13, title: "Jurgen Ekkelenkamp", desc: "<b>Jurgen Ekkelenkamp</b><br>5 april 2000 i Zeist, Nederländerna", img: "https://raw.githubusercontent.com/LiveIce/collector-data/refs/heads/main/fotboll/topps/Kort13.jpg", extra: "Mittfältare - Hertha Berlin" },
@@ -1068,6 +1070,7 @@ const data = {
     ]
   }
 };
+
 
 
 
